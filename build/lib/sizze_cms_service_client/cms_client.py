c5cf22058f744d30ab23caa1c870fd97daaffb26
kabiljanz <kabiljanz0301@gmail.com>
2022-11-15 17:16:38 +0600

    first commitgit branch -M main

--- build/lib/sizze_cms_service_client/cms_client.py
+++ build/lib/sizze_cms_service_client/cms_client.py
@@ -1,7 +1,17 @@
-from api import fields, storage, tables, values
+from api import fields, storage, tables, values, copy
 
 
-field_client = fields.FieldsClient
-storage_client = storage.StorageClient
-table_client = tables.TableClient
-value_client = values.ValuesClient
+fields_client = None
+storage_client = None
+table_client = None
+value_client = None
+copy_client = None
+
+
+def client(base_url: str):
+    global fields_client, storage_client, table_client, value_client, copy_client
+    copy_client = copy.CopyClient(base_url=base_url)
+    fields_client = fields.FieldsClient(base_url=base_url)
+    storage_client = storage.StorageClient(base_url=base_url)
+    table_client = tables.TableClient(base_url=base_url)
+    value_client = values.ValuesClient(base_url=base_url)
