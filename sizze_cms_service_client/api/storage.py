219c58a4c777e8b2b5fccef6bbb57161b79f3f07
kabiljanz <kabiljanz0301@gmail.com>
2022-11-18 21:03:27 +0600

    add template to create_storage

--- sizze_cms_service_client/api/storage.py
+++ sizze_cms_service_client/api/storage.py
@@ -1,10 +1,13 @@
 import aiohttp
 from sizze_cms_service_client.api.collection import CmsClient
+from sizze_cms_service_client.api.tables import TableClient
+from sizze_cms_service_client.api.fields import FieldsClient
+from sizze_cms_service_client.api.values import ValuesClient
 
 
 class StorageClient(CmsClient):
     async def create(self, project_id: int, users: list = None, tables: list = None, index: str = None,
-                     collection_position: int = 1):
+                     collection_position: int = 1, template: dict = None):
         if tables is None:
             tables = []
         if users is None:
@@ -22,9 +25,13 @@ class StorageClient(CmsClient):
             ) as response:
                 response_body = await response.json()
                 if response.status == 201:
-                    return response_body.get("_id")
+                    response = response_body.get("_id")
+                    if template:
+                        await self.template_create(template=template, storage=response)
                 else:
-                    return response_body
+                    response = response_body
+
+        return response
 
     async def retrieve(self, storage_id: str = None, project_id: str = None, collection_position: int = 1):
         params = {"collection_position": collection_position}
@@ -90,3 +97,56 @@ class StorageClient(CmsClient):
                     return True
                 else:
                     return False
+
+    async def template_create(self, template, storage):
+        for table in template:
+            table_client = TableClient(base_url=self.base_url)
+            table_create = await table_client.create(storage_id=storage, table_name=table.get("name"))
+            print(table_create)
+            if table_create:
+                fields = table.get("fields")
+                values = table.get("values")
+                if isinstance(fields, list) and len(fields) > 0:
+                    field_client = FieldsClient(base_url=self.base_url)
+                    for field in fields:
+                        field["table"] = table_create
+                        if field["field"] is not None:
+                            to_table = field["field"]["to_table"].split(".")
+                            field["field"]["to_table"] = template[to_table[0]][to_table[1]]
+                        print("table: ", table_create)
+                        print("field: ", field)
+                        field_create = await field_client.create(
+                            table=table_create, field=field["field"], name=field["name"]
+                        )
+                        print(field_create)
+                        field["_id"] = field_create
+
+                if isinstance(values, list) and len(values) > 0:
+                    value_client = ValuesClient(base_url=self.base_url)
+                    for value in values:
+                        value["table"] = table_create
+                        for key, val in value["values"].items():
+                            new_key = key.strip(".")
+                            new_val = val.strip(".")
+                            if len(new_key) > 1:
+                                table = next(item for item in template if item["name"] == new_key[0])
+                                new_key = table[new_key[1]][new_key[2]][new_key[3]]
+                            else:
+                                new_key = key
+
+                            if len(new_val) > 1:
+                                table = next(item for item in template if item["name"] == new_val[0])
+                                new_val = table[new_val[1]][new_val[2]][new_val[3]]
+                            else:
+                                new_val = val
+
+                            del value["values"][key]
+                            value["values"][new_key] = new_val
+
+                        print("table: ", table_create)
+                        print("values ", value.get("values"))
+                        value_create = await value_client.create(
+                            values=value.get("values"), table_id=table_create
+                        )
+                        print(value_create)
+                        value["_id"] = value_create
