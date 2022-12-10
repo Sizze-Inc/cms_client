963810e28301f39a1e625d0adbf93bdd8c8a6325
kabiljanz <kabiljanz0301@gmail.com>
2022-12-10 21:05:59 +0600

    change init method

--- build/lib/sizze_cms_service_client/api/collection.py
+++ build/lib/sizze_cms_service_client/api/collection.py
@@ -3,8 +3,8 @@ class CmsClient:
     def __init__(self):
         self.base_url = None
 
-    async def get_base_url(self):
+    def get_base_url(self):
         return self.base_url
 
-    async def set_base_url(self, base_url):
+    def set_base_url(self, base_url):
         self.base_url = base_url
