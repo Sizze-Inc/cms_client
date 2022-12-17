b79a971bec861ccd9fb01f1d7736b27d03238f2e
kabiljanz <kabiljanz0301@gmail.com>
2022-12-17 20:10:15 +0600

    fixed auth

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="sizze_cms_client",
     packages=find_packages(),
-    version='0.4.0',
-    description='add auth',
+    version='0.4.1',
+    description='fixed auth',
     author='kabu1zhan',
     license='Sizze',
 )
