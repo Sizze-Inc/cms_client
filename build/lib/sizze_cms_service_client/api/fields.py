18a2fb724b114ba5aaccae970b6b5f7fb4c436e0
kabiljanz <kabiljanz0301@gmail.com>
2023-01-29 23:13:04 +0600

    fixed 0.5.2

--- build/lib/sizze_cms_service_client/api/fields.py
+++ build/lib/sizze_cms_service_client/api/fields.py
@@ -18,11 +18,6 @@ class FieldsClient(CmsClient):
         response = await self.send_request(method="get", table_id=table_id, skip=skip, limit=limit, filtering=filtering)
         return response
 
-    async def related_list(self, table_id) -> ServerResponse:
-        self.path = "field/list_related/"
-        response = await self.send_request(method="get", table_id=table_id)
-        return response
-
     async def update(self, field_id: str, data: dict) -> ServerResponse:
         self.path = f"field/{field_id}/update/"
         response = await self.send_request(method="put", data=data)
@@ -33,35 +28,20 @@ class FieldsClient(CmsClient):
         response = await self.send_request(method="delete", field_id=field_id)
         return response
 
-    async def get_field_path(self, from_table, field_id, field_object=None):
-        if field_object:
-            table_id = field_object.get("table")
-        else:
-            field = await self.retrieve(field_id=field_id)
-            table_id = field.data.get("table")
-
-        table_path = await tables.table_client.get_table_path(from_table=from_table, to_table=table_id)
-        table_path_iterator = iter(table_path)
-        if len(table_path) > 0:
-            field_path = []
-            next_item = next(table_path_iterator)
-            for index in range(len(table_path)-1):
-                this_item, next_item = next_item, next(table_path_iterator)
-                field_path.append(
-                    {
-                        "table": {"id": this_item["table"], "index": this_item["table"]},
-                        "field": {"id": next_item["field"], "index": next_item["field"]}
-                    }
-                )
-            field_path.append(
-                {
-                    "table": {"id": table_id, "index": table_id},
-                    "field": {"id": field_id, "index": field_id}
-                }
-            )
-        else:
-            field_path = []
+    async def get_field_path(self, from_field, to_field):
+        self.path = f"field/retrieve_path/"
+        field_path = await self.send_request(method="get", from_field=from_field, to_field=to_field)
         return field_path
+
+    async def list_related(self, table_id):
+        self.path = f"field/list_related/"
+        fields = await self.send_request(method="get", table_id=table_id)
+        return fields
+
+    async def retrieve_related(self, field_id):
+        self.path = f"field/retrieve_related/"
+        fields = await self.send_request(method="get", field_id=field_id)
+        return fields
 
 
 field_client = FieldsClient()
